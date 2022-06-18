4a09775a666dbcf43c5a96e558e88efd18fa45e6
nargok <nargok@gmail.com>
2022-06-18 21:32:58 +0900

    refresh after post task

--- pages/api/task/register.ts
+++ pages/api/task/register.ts
@@ -2,10 +2,6 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// type Data = {
-//   name: string;
-// };
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,19 +1,25 @@
 import type { NextPage } from "next";
 import axios from "axios";
+import useSWR from "swr";
 import TaskForm from "./component/task/TaskForm";
 import TaskList from "./component/task/TaskList";
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 const Todo: NextPage = () => {
+  const { data, error, mutate } = useSWR(`api/task/list`, fetcher);
+
   const postTask = async (taskTitle: string) => {
     const response = await axios.post("api/task/register", {
       title: taskTitle,
     });
+    mutate("api/task/list");
   };
 
   return (
     <>
       <TaskForm postTask={postTask}></TaskForm>
-      <TaskList></TaskList>
+      <TaskList taskList={data?.data?.task_list}></TaskList>
     </>
   );
 };
