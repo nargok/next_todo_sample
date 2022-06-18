e719ceb22521c06e2a540d2ce32d19b1a753ed2e
nargok <nargok@gmail.com>
2022-06-18 21:05:55 +0900

    remove print debug

--- pages/api/task/list.ts
+++ pages/api/task/list.ts
@@ -13,9 +13,3 @@ export default async function handler(
   const data = await response.json();
   res.status(200).json({ data });
 }
-
-// export default async function handler(req, res) {
-//   const response = await fetch('https://jsonplaceholder.typicode.com/users/')
-//   const users = await response.json()
-//   res.status(200).json({ users })
-// }
--- pages/todo.tsx
+++ pages/todo.tsx
@@ -3,20 +3,11 @@ import axios from "axios";
 import TaskForm from "./component/task/TaskForm";
 import TaskList from "./component/task/TaskList";
 
-// const postFetcher = (url: string) => post(url, params).then((res) => res.json());
-
-// const res = await axios.post("/user", {
-//   id: 123,
-//   name: "Yamada Tarou",
-// });
-
 const Todo: NextPage = () => {
   const postTask = async (taskTitle: string) => {
-    // console.log("taskTitle is", taskTitle);
     const response = await axios.post("api/task/register", {
       title: taskTitle,
     });
-    // console.log("in todo page", response);
   };
 
   return (
